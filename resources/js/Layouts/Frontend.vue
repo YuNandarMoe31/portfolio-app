5a2f330684588476ce457adb5d0bde0050d7fbf1
Kaung Myat Thway <kaung.myatthwe@seattleconsultingmyanmar.com>
2022-09-30 23:52:14 +0630

    Frontend Layout

---
+++ resources/js/Layouts/Frontend.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+
+</script>
+
+<template>
+    <div class="bg-slate-200 dark:bg-slate-900">
+        <!-- Header -->
+        <Header />
+        <main class="min-h-screen">
+            <slot />
+
+        </main>
+        <!-- Footer -->
+    </div>
+</template>
