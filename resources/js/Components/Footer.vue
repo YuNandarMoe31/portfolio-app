8719debcd69584c9e8a3d4b7cee338422f12253e
YuNandarMoe31 <yunandarmoe31@gmail.com>
2022-10-01 10:56:03 +0630

    Refactor Header & Footer

--- resources/js/Components/Footer.vue
+++ resources/js/Components/Footer.vue
@@ -8,7 +8,11 @@
         <div class="container mx-auto">
             <div class="flex flex-col md:flex-row space-y-6 lg:space-y-0 items-center justify-between">
                 <div>
-                    <img src="http://portfolio.test/img/logo.png" alt="Laraveller.com" class="h-8 w-auto">
+                    <img
+                        src="https://flowbite.com/docs/images/logo.svg"
+                        class="h-8 w-auto"
+                        alt="Flowbite Logo"
+                    />
                 </div>
                 <p class="text-paragraph opacity-80 text-[15px]">
                     &copy; 2022 Laraveller.com All Right Reserved.
